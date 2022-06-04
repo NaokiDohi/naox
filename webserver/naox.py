d8c2e11dfc79a5f2b1e7db6650b0ca4488943bf4
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2022-06-04 23:45:21 +0900

    Delivery of static files(HTML)

--- webserver/naox.py
+++ webserver/naox.py
@@ -1,3 +1,4 @@
+import os
 import socket
 from datetime import datetime
 
@@ -5,6 +6,14 @@ class Naox:
     """
     TCP通信を行うサーバーを表すクラス
     """
+
+    # 実行ファイルのあるディレクトリ
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    # 静的配信するファイルを置くディレクトリ
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    print(f'BASE_DIR: {BASE_DIR}')
+    print(f'STATIC_ROOT: {STATIC_ROOT}')
+
     def serve(self):
         """
         サーバーを起動する
@@ -42,22 +51,41 @@ class Naox:
             # クライアントから送られてきたデータをファイルに書き出す
             with open("server_recv.txt", "wb") as f:
                 f.write(request)
-            
-            # レスポンスボディを生成
-            response_body = "<html><body><h1>It works!</h1></body></html>"
+
+            # リクエスト全体を
+            # 1. リクエストライン(1行目)
+            # 2. リクエストヘッダー(2行目〜空行)
+            # 3. リクエストボディ(空行〜)
+            # にパースする
+            request_line, remain = request.split(b"\r\n", maxsplit=1)
+            request_header, request_body = remain.split(b"\r\n\r\n", maxsplit=1)
+
+            # リクエストラインをパースする
+            method, path, http_version = request_line.decode().split(" ")
+
+            # pathの先頭の/を削除し、相対パスにする
+            # 消去するのはos.path.join(base, path)の仕様上　
+            # 第2引数pathに/で始まる絶対パスを与えると第一引数baseが無視される
+            relative_path = path.lstrip("/")
+            # ファイルのpathを取得
+            static_file_path = os.path.join(self.STATIC_ROOT, relative_path)
+
+            # ファイルからレスポンスボディを生成
+            with open(static_file_path, "rb") as f:
+                response_body = f.read()
 
             # レスポンスラインを生成
             response_line = "HTTP/1.1 200 OK\r\n"
             # レスポンスヘッダーを生成
             response_header = ""
             response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
-            response_header += "Host: Naox/0.1\r\n"
-            response_header += f"Content-Length: {len(response_body.encode())}\r\n"
+            response_header += "Host: Naox/0.2\r\n"
+            response_header += f"Content-Length: {len(response_body)}\r\n"
             response_header += "Connection: Close\r\n"
             response_header += "Content-Type: text/html\r\n"
 
-            # ヘッダーとボディを空行でくっつけた上でbytesに変換し、レスポンス全体を生成する
-            response = (response_line + response_header + "\r\n" + response_body).encode()
+            # ヘッダーとボディを空行で結合した後bytesに変換し、レスポンス全体を生成
+            response = (response_line + response_header + "\r\n").encode() + response_body
 
             # クライアントへレスポンスを送信する
             client_socket.send(response)
