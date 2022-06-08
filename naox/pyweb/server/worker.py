dfa98c5cb5def0b36681981fc644af00363001be
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2022-06-08 23:19:30 +0900

    Creating login function with Cookie

--- naox/pyweb/server/worker.py
+++ naox/pyweb/server/worker.py
@@ -29,6 +29,7 @@ class Worker(Thread):
     # ステータスコードとステータスラインの対応
     STATUS_LINES = {
         200: "200 OK",
+        302: "302 Found",
         404: "404 Not Found",
         405: "405 Method Not Allowed",
     }
@@ -156,7 +157,7 @@ class Worker(Thread):
         response_header += f"Content-Length: {len(response.body)}\r\n"
         response_header += "Connection: Close\r\n"
         response_header += f"Content-Type: {response.content_type}\r\n"
-        
+
         for header_name, header_value in response.headers.items():
             response_header += f"{header_name}: {header_value}\r\n"
 
